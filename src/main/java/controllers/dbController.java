86483e18445700fda3e145596626d4956dcf915d
JoshElgar <joshelgar@gmail.com>
2017-12-04 10:36:17 +0000

    db portal functionality
    db portal sync functionality

--- src/main/java/controllers/dbController.java
+++ src/main/java/controllers/dbController.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
@@ -17,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -28,7 +30,7 @@ import repositories.BlockRepository;
 import repositories.TransactionRepository;
 
 @Controller
-public class dbController {
+public class DbController {
 
 	@Autowired
 	BlockRepository blockRepo;
@@ -36,48 +38,59 @@ public class dbController {
 	@Autowired
 	TransactionRepository transactionRepo;
 
-	@RequestMapping(value = "/testportal")
-	public String servePage(Model m) {
-		System.out.println("Serving testportal");
-		return "dbPortal";
-	}
-	
-	@RequestMapping(value = "/testportal/triggersync")
-	public void triggerSync() {
-		sync();
-	}
+	@RequestMapping(value = "/testportal/sync")
+	@ResponseBody
+	public String sync() {
 
-	public void sync() {
+		System.out.println("Syncing block.");
 		// get json blocks (+ transactions)
 		CloseableHttpClient httpclient = HttpClients.createDefault();
 
 		HttpGet httpgetBlock = new HttpGet(
-				"http://localhost:8332/rest/block/0000000000000000000e9c40575a5e343f1166954d92c82ccac77d31e4d3f74f.json");
+				"http://localhost:8332/rest/block/000000000000000000af15c51a1efde4f2078ccc2d7a8a53d32c48b118027878.json");
 
 		CloseableHttpResponse resp;
 
 		try {
+
 			resp = httpclient.execute(httpgetBlock);
+			if (resp.getStatusLine().getStatusCode() == 404) {
+				return "Block not found.";
+			}
+
 			String blockInfo = EntityUtils.toString(resp.getEntity());
 
-			// Register an adapter to manage the date types as long values
-			GsonBuilder gsonBuilder = new GsonBuilder().registerTypeAdapter(Date.class, new BlockDateDeserialiser());
+			if (blockInfo.contains("Verifying blocks")) {
+				return "Bitcoind still syncing with network.";
+			} else {
 
-			Gson gson = gsonBuilder.create();
+				// Register an adapter to manage the date types as long values
+				GsonBuilder gsonBuilder = new GsonBuilder().registerTypeAdapter(Date.class,
+						new BlockDateDeserialiser());
 
-			Block generatedBlock = gson.fromJson(blockInfo, Block.class);
-			
-			System.out.println("Saving block with hash:" + generatedBlock.getHash());
-			
-			blockRepo.save(generatedBlock);
-			
-			System.out.println("Saved");
+				Gson gson = gsonBuilder.create();
+
+				Block generatedBlock = gson.fromJson(blockInfo, Block.class);
+
+				System.out.println("Saving block with hash:" + generatedBlock.getHash());
+
+				blockRepo.save(generatedBlock);
+
+				System.out.println("Saved");
+				System.out.println("Finished Syncing");
+
+			}
+
+			return "Finished syncing.";
+		} catch (HttpHostConnectException e) {
+			e.printStackTrace();
+			return "Failed to connect to Bitcoind.";
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		// deserialise into Block
-		// save block
+		return "Failed to sync.";
+
 	}
 
 	@RequestMapping(value = "/testportal/save", method = RequestMethod.GET)
